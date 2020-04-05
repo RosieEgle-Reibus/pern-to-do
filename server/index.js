fe03cffe7479df0468d736f839f93a4892940870
Rose Egle <rosiemegle89@gmail.com>
2020-04-05 13:14:23 -0400

    built out restful api w/postgres, full crud tested in postman

--- server/index.js
+++ server/index.js
@@ -6,8 +6,67 @@ const pool = require("./db")
 //middleware
 app.use(cors())
 
-//allows access to request.body to get data from clientside
-app.use(express.json())
+//allows access to req.body to get data from clientside
+app.use(express.json()) //req.body
+
+//ROUTES
+
+//create a todo
+app.post("/todos", async(req, res) => {
+    try {
+        const {description} = req.body;
+        //uses pg library to add dynamic data
+        const newToDo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description])
+        res.json(newToDo.rows[0])
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+//get all todos
+app.get("/todos", async(req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo")
+        res.json(allTodos.rows)
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+//get a todo
+app.get("/todos/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+        res.json(todo.rows[0])
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+//update todo 
+app.put("/todos/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+        const {description} = req.body
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+        res.json("Todo was updated")
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+//delete todo 
+app.delete("/todos/:id", async(req, res) => {
+    try {
+        const {id} = req.params
+        const deleteTodo = await  pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+        res.json("Todo was deleted")
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
