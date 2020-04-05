1306f32f90c413d0895d245065630754e9756ddb
Rose Egle <rosiemegle89@gmail.com>
2020-04-05 15:38:27 -0400

    enabled delete button and refresh on delete

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -19,6 +19,17 @@ const ListTodos = () => {
         getTodos()
     }, [])
 
+    //delete todo
+    const deleteTodo = async (id)  => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                 method: "DELETE"
+            }) 
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
     
     return( <Fragment>
         {" "}
@@ -32,10 +43,11 @@ const ListTodos = () => {
     </thead>
     <tbody>
         {todos.map(todo => (
-            <tr>
+            <tr key={todo.todo_id}>
                 <td>{todo.description}</td>
                 <td>Edit</td>
-                <td>Delete</td>
+                <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>
+                    Delete</button></td>
             </tr>
     ))}
     </tbody>
